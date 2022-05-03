e74c9cd051bd3a6e4b2ed54233db0f16e0ae5ee6
apolinario <joaopaulo.passos@gmail.com>
2022-05-03 17:21:22 +0200

    Invert where to add

--- src/dalle_mini/model/processor.py
+++ src/dalle_mini/model/processor.py
@@ -51,7 +51,7 @@ class DalleBartProcessorBase:
     def from_pretrained(cls, *args, **kwargs):
         tokenizer = DalleBartTokenizer.from_pretrained(*args, **kwargs)
         config = DalleBartConfig.from_pretrained(*args, **kwargs)
-        return cls(tokenizer, config.normalize_text, config.max_text_length, low_cpu_mem_usage=True)
+        return cls(tokenizer, config.normalize_text, config.max_text_length)
 
 
 class DalleBartProcessor(PretrainedFromWandbMixin, DalleBartProcessorBase):
